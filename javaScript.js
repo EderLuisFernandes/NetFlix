553f8ff46e0fbc4cf6ec7bb6d01cffe13503f675
Ederl010203 <eder.luis.1996@hotmail.com>
2024-11-04 22:30:27 -0300

    adicione minha assinatura mais algumas coisas

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,17 @@
+const btnInfo = document.querySelector("#link-info")
+const modal = document.querySelector('#modal')
+const close =  document.querySelector(".close")
+const btnSonModal =document.querySelector('.botao-som-modal')
+const videoModal = document.querySelector('#video-modal')
+
+const btnSon = document.querySelector('.botao-som');
+let video = document.querySelector("#video");
+
 const swiper = new Swiper('.slider-wrapper', {
     loop: true,
     grabCursor: true,
     slidesPerView: 'auto',
     spaceBetween: 10,
-  
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   clickable:true,
-    //   dynamicBullets: true
-    // },
-  
    
     navigation: {
       nextEl: '.swiper-button-next',
@@ -28,3 +30,48 @@ const swiper = new Swiper('.slider-wrapper', {
     }
    
   });
+  btnSon.addEventListener("click", ()=>{
+
+    if(video.muted == true ){
+       video.muted = false
+       btnSon.querySelector(".ph-speaker-x").style.display = "none" 
+       btnSon.querySelector(".ph-speaker-high").style.display = "inline" 
+    }else{
+      video.muted = true
+        btnSon.querySelector(".ph-speaker-x").style.display = "inline" 
+       btnSon.querySelector(".ph-speaker-high").style.display = "none" 
+     
+    }
+    
+  })
+
+  btnSonModal.addEventListener('click' ,()=>{
+    if(videoModal.muted == true){
+      videoModal.muted = false
+      btnSonModal.querySelector('.ph-speaker-x').style.display = 'none'
+      btnSonModal.querySelector('.ph-speaker-high').style.display = 'inline'
+    }else{
+      videoModal.muted = true
+      btnSonModal.querySelector('.ph-speaker-x').style.display = 'inline'
+      btnSonModal.querySelector('.ph-speaker-high').style.display = 'none'
+    }
+  })
+  btnInfo.addEventListener('click',()=>{
+    modal.style.display = 'block'
+    video.currentTime = 0
+    video.pause()
+    
+  })
+
+  close.addEventListener('click', ()=>{
+    modal.style.display = 'none'
+    video.currentTime = 0
+    video.play()
+  })
+  
+  window.onload  = ()=>{
+    setTimeout(()=>{
+      const audio = document.querySelector('#audio')
+      audio.play()
+    }, 4000)
+  }
